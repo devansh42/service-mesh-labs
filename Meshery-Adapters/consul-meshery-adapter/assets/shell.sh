42ddc2adc8d0d2c2b7143470978a47e29e0ef9fa
Devansh Gupta <devanshguptamrt@gmail.com>
2021-08-10 00:33:00 +0530

    added fix for foreground script

--- Meshery-Adapters/consul-meshery-adapter/assets/shell.sh
+++ Meshery-Adapters/consul-meshery-adapter/assets/shell.sh
@@ -7,8 +7,8 @@ while [ ! -e $HOME/.kube/config ];do
     cd $HOME && minikube start
 done
 echo "minikube installed"
-echo "Almost done..."
 sleep 10s
-cd $HOME/.kube && kubectl config view --minify --flatten > config
+echo "Almost done..."
 
+cd $HOME/.kube && kubectl config view --minify --flatten > config
 echo "Your environment has been set up with a Minikube cluster. You may now begin the tutorial"
